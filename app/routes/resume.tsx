e183b80df6b540d47a19a2505944fca6bfabdd41
zulhanarif <102965995+SullKanaeru@users.noreply.github.com>
2025-08-01 00:10:01 +0900

    Resume Feedback Component

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -64,46 +64,46 @@ const Resume = () => {
                             Back to Homepage
                         </span>
                     </Link>
-                    <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                        <section className="feedback-section bg-[url('/images/bg-small.svg) bg-cover h-[100vh] sticky top-0 items-center justify-around">
-                            {imageUrl && resumeUrl && (
-                                <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
-                                    <a
-                                        href={resumeUrl}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <img
-                                            src={imageUrl}
-                                            className="w-full h-full object-contain rounded-2xl"
-                                            title="resume"
-                                        />
-                                    </a>
-                                </div>
-                            )}
-                        </section>
-                        <section className="feedback-section">
-                            <h2 className="text-4xl !text-black font-bold">
-                                resume review
-                            </h2>
-                            {feedback ? (
-                                <div className="flex flex-col gap-8 animate-on fade-in duration-1000">
-                                    <Summary feedback={feedback} />
-                                    <ATS
-                                        score={feedback.ATS.score || 0}
-                                        suggestions={feedback.ATS.tips || []}
-                                    />
-                                    <Details feedback={feedback} />
-                                </div>
-                            ) : (
-                                <img
-                                    src="/images/resume-scan-2.gif"
-                                    className="w-full"
-                                />
-                            )}
-                        </section>
-                    </div>
                 </nav>
+                <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                    <section className="feedback-section bg-[url('/images/bg-small.svg) bg-cover h-[100vh] sticky top-0 items-center justify-around">
+                        {imageUrl && resumeUrl && (
+                            <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                                <a
+                                    href={resumeUrl}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <img
+                                        src={imageUrl}
+                                        className="w-full h-full object-contain rounded-2xl"
+                                        title="resume"
+                                    />
+                                </a>
+                            </div>
+                        )}
+                    </section>
+                    <section className="feedback-section">
+                        <h2 className="text-4xl !text-black font-bold">
+                            resume review
+                        </h2>
+                        {feedback ? (
+                            <div className="flex flex-col gap-8 animate-on fade-in duration-1000">
+                                <Summary feedback={feedback} />
+                                <ATS
+                                    score={feedback.ATS.score || 0}
+                                    suggestions={feedback.ATS.tips || []}
+                                />
+                                <Details feedback={feedback} />
+                            </div>
+                        ) : (
+                            <img
+                                src="/images/resume-scan-2.gif"
+                                className="w-full"
+                            />
+                        )}
+                    </section>
+                </div>
             </main>
         </div>
     );
